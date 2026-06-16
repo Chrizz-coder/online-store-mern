1c552da580f70d2c28918386f2c0667f08641397
Chrizz-Coder <christinmp07@gmail.com>
2026-06-16 23:35:17 +0530

    Implement login with jwt and implemented generateToken for repetative usage in contollers

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,56 +1,85 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-
-export const registerUser = async (req,res) =>{
+import generateToken from "../utils/generateToken.js";
+export const registerUser = async (req, res) => {
   try {
-    const {name,email,password} = req.body;
+    const { name, email, password } = req.body;
 
-    if(!name || !email || !password){
-      return res.status(400).json({message:"Please enter all required field"});
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please enter all required field" });
     }
 
-    const userExists = await User.findOne({email});
-    if(userExists){
-      return res.status(409).json({message:"User already exists with the same email"});
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res
+        .status(409)
+        .json({ message: "User already exists with the same email" });
     }
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password,salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({
       name,
       email,
-      password:hashedPassword
+      password: hashedPassword,
     });
 
     await newUser.save();
 
-    if(!process.env.JWT_SECRET){
-      throw new Error("JWT_SECRET not configured!");
-      
-    }
     
-    const token = jwt.sign(
-      { id: newUser._id, role: newUser.role},
-      process.env.JWT_SECRET,
-      { expiresIn:'2d'}
-    
-    );
-  
+    const token = generateToken(newUser._id,newUser.role);
+
     return res.status(201).json({
-      message:"User registered successfully",
+      message: "User registered successfully",
       token,
-      user:{
+      user: {
         id: newUser._id,
         name: newUser.name,
         email: newUser.email,
-        role: newUser.role
-      }
+        role: newUser.role,
+      },
     });
   } catch (error) {
-    console.log("Registration error ",error);
-    return res.status(500).json({ message:"Server error, please try again."})
-    
+    console.log("Registration error ", error);
+    return res.status(500).json({ message: "Server error, please try again." });
   }
-}
+};
 
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please provide both email and password" });
+    }
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    
+    
+    const token = generateToken(user._id,user.role);
+    return res.status(200).json({
+      message: "Logged in successfully",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.error("Login error:", error)
+    return res.status(500).json({ message: "Server error during login" });
+  }
+};
--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -1,5 +1,10 @@
 import jwt from "jsonwebtoken";
 
+
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET not configured!");
+}
+
 const generateToken = (userId,role) =>{
 
   return jwt.sign(
